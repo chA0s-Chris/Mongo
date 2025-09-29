b6d5e885970f123d2a9eb2d18073690f09065419
Christian Flessa <chris@z0rn.net>
2025-09-29 15:58:14 +0200

    Add test logging
    Signed-off-by: Christian Flessa <chris@z0rn.net>

---
+++ tests/Chaos.Testing/Logging/NUnitTestLogger.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) 2025 Christian Flessa. All rights reserved.
+// This file is licensed under the MIT license. See LICENSE in the project root for more information.
+namespace Chaos.Testing.Logging;
+
+using Microsoft.Extensions.Logging;
+
+public sealed class NUnitTestLogger : ILogger
+{
+    private readonly String _categoryName;
+
+    public NUnitTestLogger(String categoryName)
+    {
+        _categoryName = String.IsNullOrEmpty(categoryName) ? nameof(NUnitTestLogger) : categoryName;
+    }
+
+    public IDisposable? BeginScope<TState>(TState state)
+        where TState : notnull
+        => DummyDisposable.Instance;
+
+    public Boolean IsEnabled(LogLevel logLevel)
+        => NUnit.Framework.TestContext.Progress is not null &&
+           logLevel != LogLevel.None;
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, String> formatter)
+    {
+        ArgumentNullException.ThrowIfNull(formatter);
+
+        if (!IsEnabled(logLevel))
+        {
+            return;
+        }
+
+        var message = formatter.Invoke(state, exception);
+        if (String.IsNullOrEmpty(message))
+        {
+            return;
+        }
+
+        var output = $"{logLevel}: {message}";
+        if (exception is not null)
+        {
+            output += $"{Environment.NewLine}{exception}";
+        }
+
+        NUnit.Framework.TestContext.Progress.WriteLine(output);
+    }
+
+    private class DummyDisposable : IDisposable
+    {
+        public static DummyDisposable Instance => new();
+
+        public void Dispose() { }
+    }
+}
---
+++ tests/Chaos.Testing/TestLogging.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) 2025 Christian Flessa. All rights reserved.
+// This file is licensed under the MIT license. See LICENSE in the project root for more information.
+namespace Chaos.Testing;
+
+using Chaos.Testing.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Collections.Concurrent;
+
+public static class TestLogging
+{
+    private static readonly ConcurrentDictionary<Type, ILogger> _loggers = new();
+
+    public static ILogger GetTestLogger()
+        => GetTestLogger<NUnitTestLogger>();
+
+    public static ILogger<T> GetTestLogger<T>()
+        => (ILogger<T>)_loggers.GetOrAdd(typeof(T), _ =>
+        {
+            using var serviceProvider = new ServiceCollection()
+                                        .AddNUnitTestLogging()
+                                        .BuildServiceProvider();
+
+            return serviceProvider.GetRequiredService<ILoggerFactory>()
+                                  .CreateLogger<T>();
+        });
+}
