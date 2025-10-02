ee81d122771983daa2d1f3cfc15a80336d08f1a9
Christian Flessa <chris@z0rn.net>
2025-10-02 10:22:02 +0200

    Add MongoQueueCollectionNameGenerator
    Signed-off-by: Christian Flessa <chris@z0rn.net>

---
+++ src/Chaos.Mongo/Queues/MongoQueueCollectionNameGenerator.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) 2025 Christian Flessa. All rights reserved.
+// This file is licensed under the MIT license. See LICENSE in the project root for more information.
+namespace Chaos.Mongo.Queues;
+
+using System.IO.Hashing;
+using System.Text;
+
+public class MongoQueueCollectionNameGenerator : IMongoQueueCollectionNameGenerator
+{
+    private const String NamePrefix = "_Queue.";
+
+    public Task<String> GenerateQueueCollectionName(Type payloadType)
+    {
+        ArgumentNullException.ThrowIfNull(payloadType);
+
+        var builder = new StringBuilder(NamePrefix);
+
+        if (!String.IsNullOrEmpty(payloadType.FullName))
+        {
+            var hash = XxHash3.Hash(Encoding.UTF8.GetBytes(payloadType.FullName));
+            var hexString = Convert.ToHexString(hash);
+
+            builder.Append(hexString);
+            builder.Append('.');
+        }
+
+        builder.Append(payloadType.Name);
+
+        return Task.FromResult(builder.ToString());
+    }
+}
