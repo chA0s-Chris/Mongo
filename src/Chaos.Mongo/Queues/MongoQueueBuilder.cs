6f45230e4bf244ee8df9046b2e2f5661a3f7941a
Christian Flessa <chris@z0rn.net>
2025-10-02 10:23:36 +0200

    Add draft MongoQueueBuilder
    Signed-off-by: Christian Flessa <chris@z0rn.net>

---
+++ src/Chaos.Mongo/Queues/MongoQueueBuilder.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) 2025 Christian Flessa. All rights reserved.
+// This file is licensed under the MIT license. See LICENSE in the project root for more information.
+namespace Chaos.Mongo.Queues;
+
+using Microsoft.Extensions.DependencyInjection;
+
+public sealed class MongoQueueBuilder<TPayload>
+    where TPayload : class, new()
+{
+    private readonly IServiceCollection _services;
+
+    public MongoQueueBuilder(IServiceCollection services)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+        _services = services;
+    }
+}
